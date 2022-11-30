e0b3123df0736f76dade0cbe8898893d812064e1
Kirk Dirk <jackalex@ya.ru>
2022-12-01 00:37:59 +0500

    add #4

---
+++ HW_t4.java
@@ -0,0 +1,26 @@
+package Sem.date221121.HW2;
+
+public class HW_t4 {
+    public static void main(String[] args) {
+
+// 4. Дано два числа, например 3 и 56, 
+// необходимо составить следующие строки: 3 + 56 = 59 3 – 56 = -53 3 * 56 = 168 Используем метод StringBuilder.append().        
+
+        int num1 = 3; int num2 = 56;
+        StringBuilder result = new StringBuilder("");
+        result.append(num1);
+        result.append(" + ");
+        result.append(num2);
+        result.append(" = ");
+        result.append(num1 + num2);
+        System.out.println(result);
+        result.setLength(0);
+        result.append(num1 + " - " + num2 + " = " + (num1-num2));
+        System.out.println(result);
+        result.setLength(0);
+        result.append(num1 + " * " + num2 + " = " + (num1*num2));
+        System.out.println(result);
+        
+
+    }
+}
