4fe71e9377b452809be60b6a82881c35feaf1bd4
Kirk Dirk <jackalex@ya.ru>
2022-12-01 00:07:04 +0500

    add #2. #1 не выходит:(((

--- HW_t1.java
+++ HW_t1.java
@@ -12,6 +12,7 @@ package Sem.date221121.HW2;
 import java.util.Scanner;
 
 public class HW_t1 {
+    static final int no_of_chars = 256;
     
     // 1. Напишите программу на Java, чтобы найти наименьшее окно в строке, содержащей все символы другой строки.
     public static void main(String[] args) {
@@ -26,42 +27,56 @@ public class HW_t1 {
         for (int i = 0; i < pattern_str.length(); i++) {
             if (input_string.indexOf(pattern_str.charAt(i)) == -1) cond = false;
         }
-        if (cond == false) {
+        // Отбрасываем варианты когда хотя бы одного искомого символа нет в строке 
+        // или длина начальной строки меньше искомой 
+        if (cond == false | input_string.length()<pattern_str.length()) {
             System.out.println("Решения не найдено");
         } else {
-            int size_wndw = 0;
-            int start_wndw = input_string.length();
-            int end_wndw = 0;
+            int start_wndw = 0;
+            int start_index = -1;
+            int size_wndw = input_string.length();
+            
+            int arr_str[] = new int[no_of_chars];
+            int arr_pat[] = new int[no_of_chars];
+            
+            for (int i = 0; i < pattern_str.length(); i++) arr_pat[pattern_str.charAt(i)]++;
 
-            for (int i = 0; i < pattern_str.length(); i++) {
-                for (int j = 0; j < input_string.length(); j++) {
-                    if (input_string.charAt(j)==pattern_str.charAt(i)) {
-                        int min_char = j;
-                        int max_char = 0;
-                        for (int k = i; k < pattern_str.length(); k++) {
-                            if (max_char < input_string.indexOf(pattern_str.charAt(k), j)) {
-                                min_char = j;
-                                max_char = input_string.indexOf(pattern_str.charAt(k));
-                                if (size_wndw < max_char-min_char) {
-                                    size_wndw = max_char-min_char;
-                                    start_wndw = min_char;
-                                    end_wndw = max_char;
-                                }
-                            }
-                            
-                        }
-                        
-                    }    
+            // Начальное значение счетчика совпадений символов в строке
+            int count = 0;
+            for (int j = 0; j < input_string.length(); j++) {
+                // Считаем вхождение символов из строки
+                arr_str[input_string.charAt(j)]++;
+                
+                // Если символ строки совпадает с шаблоном, увеличиваем счетчик
+                if (arr_str[input_string.charAt(j)] <= arr_pat[input_string.charAt(j)]) count++;
+                
+                // Если все символы совпадают
+                if (count == pattern_str.length()) {
+                    // Пробуем минимизировать окно
+                    while (arr_str[input_string.charAt(start_wndw)] > arr_pat[input_string.charAt(start_wndw)]
+                       || arr_pat[input_string.charAt(start_wndw)] == 0) {
+ 
+                        if (arr_str[input_string.charAt(start_wndw)] > arr_pat[input_string.charAt(start_wndw)])
+                            arr_str[input_string.charAt(start_wndw)]--;
+                        start_wndw++;
+                    }
+ 
+                    // Обновляем размер окна
+                    int len_wndw = j - start_wndw + 1;
+                    if (size_wndw > len_wndw) {
+                        size_wndw = len_wndw;
+                        start_index = start_wndw;
+                    }
                 }
             }
-
-            String res_str = input_string.substring(start_wndw-1, end_wndw);
+                            
+            String res_str = input_string.substring(start_wndw-1, start_wndw+size_wndw); // return str.substring(start_index, start_index + min_len);
             System.out.printf("Окно <");
             System.out.printf(res_str);
             System.out.printf("> содержит все символы искомого набора: ");
             System.out.printf(pattern_str);    
-            iScanner.close();
         }        
+        iScanner.close();
     }
 } 
 
--- HW_t2.java
+++ HW_t2.java
@@ -1,20 +1,29 @@
 package Sem.date221121.HW2;
 
-// import java.util.Scanner;
+import java.util.Scanner;
 
 public class HW_t2 {
     // 2. Напишите программу на Java, чтобы проверить, являются ли две данные строки вращением друг друга.
     public static void main(String[] args) {
                        
-        // Scanner iScanner = new Scanner(System.in);
-        // System.out.printf("Введите строку: ");
-        // String one_string = iScanner.nextLine();
-        // System.out.printf("Введите вторую строку: ");
-        // String two_string = iScanner.nextLine();
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите строку: ");
+        String one_string = iScanner.nextLine();
+        System.out.printf("Введите вторую строку: ");
+        String two_string = iScanner.nextLine();
         
+        boolean spin = true;
+        if (one_string.length() != two_string.length()) spin = false;
+        else { 
+            for (int i = 0; i < one_string.length(); i++) {
+            if (one_string.charAt(i) != two_string.charAt(two_string.length()-i-1)) spin = false;
+            }
+        }
+        System.out.printf("Первая и вторая строка ");
+        String result = "не являются вращением друг друга ";
+        if (spin) result = "являются вращением друг друга";
+        System.out.printf(result);
 
-
-
-        // iScanner.close();
+        iScanner.close();
     }
 }
